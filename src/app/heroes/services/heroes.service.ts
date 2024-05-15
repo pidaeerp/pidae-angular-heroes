2d4479417d8caaeb13acb5684ffb1770d82fa51e
Oscar R. Vásquez Alfaro <pidaesisfocom@outlook.com>
2024-05-15 17:21:17 -0500

    CRUD

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, catchError, of } from 'rxjs';
+import { Observable, catchError, map, of } from 'rxjs';
 import { environments } from '../../../enviroments/environmets';
 import { Hero } from '../interfaces/hero.interface';
 
@@ -22,6 +22,26 @@ export class HeroesService {
   getSuggestions(query: string): Observable<Hero[]> {
     return this.httpClient.get<Hero[]>(
       `${this.baseUrl}/heroes?q=${query}&_limit=6`
+    );
+  }
+
+  //CRUD
+  addHero(hero: Hero): Observable<Hero> {
+    return this.httpClient.post<Hero>(`${this.baseUrl}/heroes`, hero);
+  }
+
+  updateHero(hero: Hero): Observable<Hero> {
+    if (!hero.id) throw Error('Hero id is required');
+    return this.httpClient.patch<Hero>(
+      `${this.baseUrl}/heroes/${hero.id}`,
+      hero
+    );
+  }
+
+  deleteHero(id: string): Observable<boolean> {
+    return this.httpClient.delete(`${this.baseUrl}/heroes/${id}`).pipe(
+      map((resp) => true),
+      catchError((err) => of(false))
     );
   }
 }
